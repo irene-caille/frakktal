9051f927e8de25ef56f2d7fd35efbb57029fffd9
Irene <52959944+irene-caille@users.noreply.github.com>
2024-08-21 18:08:57 +0200

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,34 @@
+import mdx from '@astrojs/mdx'
+import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import keystatic from '@keystatic/astro'
+import playformCompress from '@playform/compress'
+import { defineConfig } from 'astro/config'
+import settings from './settings.json'
+import rehypeMathjax from "rehype-mathjax"
+import netlify from '@astrojs/netlify'
+
+// https://astro.build/config
+export default defineConfig({
+	markdown:{
+		rehypePlugins:[rehypeMathjax]
+	},
+	site: settings.url,
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		react(),
+		mdx(),
+		keystatic(),
+		sitemap(),
+		playformCompress({
+			Image: false,
+		}),
+	],
+	output: 'hybrid',
+	adapter: netlify({
+		imageCDN: false,
+	}),
+})
